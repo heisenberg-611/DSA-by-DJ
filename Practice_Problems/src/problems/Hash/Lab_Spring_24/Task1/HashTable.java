a696471eaef095dbc53441869311087ceaddc9a3
heisenberg-611 <lnsaha1214@gmail.com>
2025-07-19 14:58:48 +0600

    hashtable task 1 done

--- Practice_Problems/src/problems/Hash/Lab_Spring_24/Task1/HashTable.java
+++ Practice_Problems/src/problems/Hash/Lab_Spring_24/Task1/HashTable.java
@@ -7,37 +7,38 @@ package problems.Hash.Lab_Spring_24.Task1;
  DO NOT TOUCH any other methods or codes*/
 public class HashTable {
 
-    //ht[] :: is the HashTable array that stores the PairNode objects
+    // ht[] :: is the HashTable array that stores the PairNode objects
     private PairNode[] ht;
 
-    //Constructor that initializes the HashTable array
-	//DO NOT change this Constructor
-    public HashTable(int size){
+    // Constructor that initializes the HashTable array
+    // DO NOT change this Constructor
+    public HashTable(int size) {
         this.ht = new PairNode[size];
     }
 
-    //This method is called to insert each pair from the 2D Array
-	//DO NOT change this method
-    public void createFromArray(Object[][] arr){
-        for( Object[] x: arr )
-            this.insert( x );
+    // This method is called to insert each pair from the 2D Array
+    // DO NOT change this method
+    public void createFromArray(Object[][] arr) {
+        for (Object[] x : arr)
+            this.insert(x);
     }
 
-    //The insert() method inserts the pair into proper Hashed Index
-	//This method is already written including collision resolve using Forward Chaining
-    //DO NOT change this method
-    public void insert(Object[] keyValuePair){
-        String key = (String)keyValuePair[0];
-        Integer value = (Integer)keyValuePair[1];
+    // The insert() method inserts the pair into proper Hashed Index
+    // This method is already written including collision resolve using Forward
+    // Chaining
+    // DO NOT change this method
+    public void insert(Object[] keyValuePair) {
+        String key = (String) keyValuePair[0];
+        Integer value = (Integer) keyValuePair[1];
         String srchResult = this.searchHashtable(keyValuePair);
-        if ( srchResult==null ) {
+        if (srchResult == null) {
             System.out.println("Incomplete searchHashTable() and hashFunction() method");
-        } else if ( srchResult.equals( "Found" ) ){
-            System.out.println("("+key+","+value+") already Inserted. Cannot reinsert.");
+        } else if (srchResult.equals("Found")) {
+            System.out.println("(" + key + "," + value + ") already Inserted. Cannot reinsert.");
         } else {
-            int hashedIndex = this.hashFunction( key );
-            PairNode newPNode = new PairNode( key, value );
-            if ( this.ht[hashedIndex] == null ){
+            int hashedIndex = this.hashFunction(key);
+            PairNode newPNode = new PairNode(key, value);
+            if (this.ht[hashedIndex] == null) {
                 this.ht[hashedIndex] = newPNode;
             } else {
                 newPNode.next = this.ht[hashedIndex];
@@ -46,69 +47,70 @@ public class HashTable {
         }
     }
 
-    //This method basically prints the HashTable
-    //DO NOT change this method
-    public void printHashTable(){
-        for(int i=0; i<ht.length; i++){
-            System.out.print( i+" : " );
+    // This method basically prints the HashTable
+    // DO NOT change this method
+    public void printHashTable() {
+        for (int i = 0; i < ht.length; i++) {
+            System.out.print(i + " : ");
             PairNode pNode = ht[i];
-            if( pNode==null ) System.out.println("null");
-            while (pNode!=null){
-                System.out.print("(Key: "+pNode.key+", Value: "+pNode.value+") --> ");
-                if (pNode.next==null) System.out.println("null");
+            if (pNode == null)
+                System.out.println("null");
+            while (pNode != null) {
+                System.out.print("(Key: " + pNode.key + ", Value: " + pNode.value + ") --> ");
+                if (pNode.next == null)
+                    System.out.println("null");
                 pNode = pNode.next;
             }
         }
     }
 
-	//you need to COMPLETE this method
-    private int hashFunction( String key ){
+    // you need to COMPLETE this method
+    private int hashFunction(String key) {
         // TO DO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
         int sum = 0;
         int len = key.length();
-        if(len % 2 != 0){ 
+        if (len % 2 != 0) {
             key += "N";
             len = key.length();
         }
-        for (int i = 0; i < len; i+=2) {
+        for (int i = 0; i < len; i += 2) {
             char c1 = key.charAt(i);
-            char c2 = key.charAt(i+1);
+            char c2 = key.charAt(i + 1);
             int ascii1 = (int) c1;
             int ascii2 = (int) c2;
-            int finalL = Integer.parseInt(""+ascii1+ascii2);
+            int finalL = Integer.parseInt("" + ascii1 + ascii2);
             sum += finalL;
         }
         return sum % ht.length;
     }
 
-
-	//you need to COMPLETE this method
-    //Hint: you may need to use Integer.parseInt() to convert from String to Integer
+    // you need to COMPLETE this method
+    // Hint: you may need to use Integer.parseInt() to convert from String to
+    // Integer
     // public String searchHashtable( Object[] keyValuePair ){
-    //     // TO DO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    // // TO DO !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-    //     //it'll return either "Found" or "Not Found"
-    //     return null; // remove this line
+    // //it'll return either "Found" or "Not Found"
+    // return null; // remove this line
     // }
     public String searchHashtable(Object[] keyValuePair) {
-    String key = (String) keyValuePair[0];
-    int index = hashFunction(key);
-    int startIndex = index;
+        String key = (String) keyValuePair[0];
+        int index = hashFunction(key);
+        int startIndex = index;
 
-    while (ht[index] != null) {
-        if (ht[index].key.equals(key)) {
-            return "Found";
+        while (ht[index] != null) {
+            if (ht[index].key.equals(key)) {
+                return "Found";
+            }
+
+            index = (index + 1) % ht.length;
+
+            if (index == startIndex) {
+                break; // full loop, key not found
+            }
         }
 
-        index = (index + 1) % ht.length;
-
-        if (index == startIndex) {
-            break; // full loop, key not found
-        }
+        return "Not Found";
     }
-
-    return "Not Found";
-}
-
 
 }
--- Practice_Problems/src/problems/Hash/Lab_Spring_24/Task1/PairNode.java
+++ Practice_Problems/src/problems/Hash/Lab_Spring_24/Task1/PairNode.java
@@ -1,16 +1,16 @@
 package problems.Hash.Lab_Spring_24.Task1;
 
 public class PairNode {
-    
+
     String key;
     Integer value;
     PairNode next;
 
-    public PairNode( String k, Integer v ){
+    public PairNode(String k, Integer v) {
         // TO DO
         this.key = k;
         this.value = v;
         this.next = null;
     }
-    
+
 }
